b9f4068902adebae4b0a96880494b2981cf8fe72
Shilu <mehediimamshilu007@gmail.com>
2022-07-22 12:22:42 +0600

    second commit

---
+++ LearnPython/07. Class/number.py
@@ -0,0 +1,12 @@
+# This code is from learnpython.org
+
+class NumberHolder:
+    def __init__(self, number):
+        self.number = number
+
+    def returnNumber(self):
+        return self.number
+
+
+var = NumberHolder(7)
+print(var.returnNumber())  # Prints '7'
